dcb5c7e45d8d8f8fd871e17bb9f1a221cc385728
lidi83 <lidi.pinheiro10@gmail.com>
2024-06-13 21:46:36 -0300

    Primeiro Commit - Projeto Mario

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function mostrarForm(){
+    form.style.left = "40%"
+    mascara.style.visibility = "visible"
+    }
+
+function esconderForm(){    
+    form.style.left = "-235%"
+    mascara.style.visibility = "hidden"
+}
+
+
+
+
+
+
+  
+    
+    
